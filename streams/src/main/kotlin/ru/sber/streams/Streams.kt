dcb6a3202b9efda8ad6d8b6b2b2bbb646006ee61
Elvina Ganieva <elvinaganieva5411@yandex.ru>
2021-10-05 11:44:22 +0300

    добавила = после имени функции и цепочку, добавила reduce в 9

--- streams/src/main/kotlin/ru/sber/streams/Streams.kt
+++ streams/src/main/kotlin/ru/sber/streams/Streams.kt
@@ -2,56 +2,46 @@ package ru.sber.streams
 
 
 // 1. Используя withIndex() посчитать сумму элементов листа, индекс которых кратен 3. (нулевой индекс тоже входит)
-fun getSumWithIndexDivisibleByThree(list: List<Long>): Long {
-    return list.asSequence()
+fun getSumWithIndexDivisibleByThree(list: List<Long>): Long =
+    list.asSequence()
         .withIndex()
         .filter { it.index % 3 == 0 }
         .sumOf { it.value }
-}
 
 // 2. Используя функцию generateSequence() создать последовательность, возвращающую числа Фибоначчи.
-fun generateFibonacciSequence(): Sequence<Int> {
-    return generateSequence(Pair(0, 1)) { Pair(it.second, it.first + it.second) }.map { it.first }
-}
+fun generateFibonacciSequence(): Sequence<Int> =
+    generateSequence(Pair(0, 1)) { Pair(it.second, it.first + it.second) }.map { it.first }
 
 // 3. Получить города, в которых есть покупатели.
-fun Shop.getCustomersCities(): Set<City> {
-    return customers.map { it.city }.toSet()
-}
+fun Shop.getCustomersCities(): Set<City> = customers.map { it.city }.toSet()
 
 // 4. Получить все когда-либо заказанные продукты.
-fun Shop.allOrderedProducts(): Set<Product> {
-    val result = customers.flatMap { customer ->
+fun Shop.allOrderedProducts(): Set<Product> =
+    customers.flatMap { customer ->
         customer.orders.flatMap { it.products }
     }.toSet()
-    return result
-}
 
 // 5. Получить покупателя, который сделал больше всего заказов.
-fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? {
-    return customers.maxByOrNull { customer -> customer.orders.size }
-}
+fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? =
+    customers.maxByOrNull { customer -> customer.orders.size }
 
 // 6. Получить самый дорогой продукт, когда-либо приобртенный покупателем.
-fun Customer.getMostExpensiveProduct(): Product? {
-    return orders.flatMap { it.products }.maxByOrNull { it.price }
-}
+fun Customer.getMostExpensiveProduct(): Product? =
+    orders.flatMap { it.products }.maxByOrNull { it.price }
 
 // 7. Получить соответствие в мапе: город - количество заказанных и доставленных продуктов в данный город.
-fun Shop.getNumberOfDeliveredProductByCity(): Map<City, Int> {
-    val result = customers.groupBy(
+fun Shop.getNumberOfDeliveredProductByCity(): Map<City, Int> =
+    customers.groupBy(
         keySelector = { it.city },
         valueTransform = {
             it.orders.asSequence()
                 .filter { order -> order.isDelivered }
                 .sumOf { order -> order.products.size }
         }).mapValues { it.value.sum() }
-    return result
-}
 
 // 8. Получить соответствие в мапе: город - самый популярный продукт в городе.
-fun Shop.getMostPopularProductInCity(): Map<City, Product> {
-    val result = customers.groupBy(
+fun Shop.getMostPopularProductInCity(): Map<City, Product> =
+    customers.groupBy(
         keySelector = { it.city },
         valueTransform = {
             it.orders.flatMap { order -> order.products }
@@ -60,16 +50,12 @@ fun Shop.getMostPopularProductInCity(): Map<City, Product> {
             .groupingBy { product: Product -> product }
             .eachCount()
     }.mapValues { it.value.maxByOrNull { product -> product.value }!!.key }
-    return result
-}
+
 
 // 9. Получить набор товаров, которые заказывали все покупатели.
-fun Shop.getProductsOrderedByAll(): Set<Product> {
-    val result = mutableSetOf<Product>()
-
+fun Shop.getProductsOrderedByAll(): Set<Product> =
     customers.associateWith {
-        it.orders.flatMap { order -> order.products }.distinct()
-    }.values.withIndex()
-        .forEach { if (it.index == 0) result.addAll(it.value) else result.retainAll(it.value) }
-    return result
-}
+        it.orders.flatMap { order -> order.products }.toSet()
+    }.values.reduce { acc, set ->
+        acc intersect set
+    }
