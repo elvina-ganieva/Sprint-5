cf0294206d41b4ee4e08231eede4709a60ed3df7
Elvina Ganieva <elvinaganieva5411@yandex.ru>
2021-10-06 11:49:01 +0300

    fix jsonCustomClassDeserializer

--- serialization/src/main/kotlin/ru/sber/serialization/Client7.kt
+++ serialization/src/main/kotlin/ru/sber/serialization/Client7.kt
@@ -1,9 +1,12 @@
 package ru.sber.serialization
 
 import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.core.ObjectCodec
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+
 
 @JsonDeserialize(using = Client7Deserializer::class)
 data class Client7(
@@ -14,8 +17,9 @@ data class Client7(
 
 class Client7Deserializer : JsonDeserializer<Client7>() {
     override fun deserialize(p0: JsonParser?, p1: DeserializationContext?): Client7 {
-        p0!!.nextTextValue()
-        val value = p0.nextTextValue()!!.trim().split("\\s+".toRegex())
-        return Client7(value[1], value[0], value[2])
+        val codec: ObjectCodec = p0!!.codec
+        val node = codec.readTree<JsonNode>(p0)
+        val str = node.findValue("client").textValue().split("\\s+".toRegex())
+        return Client7(str[1], str[0], str[2])
     }
 }
